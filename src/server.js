f70c30d7be9c73a46604b81db012cb08ca585d9d
Eduardo Kaique <dudukaique@hotmail.com>
2022-05-28 15:41:38 -0300

    Alteração para pegar porta de variável de ambiente para o Heroku.

--- src/server.js
+++ src/server.js
@@ -173,7 +173,8 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(3000, (err) => {
+const port  = process.env.PORT || 3000;
+server.listen(port, (err) => {
     if (err) {
         console.log(err);
     }
